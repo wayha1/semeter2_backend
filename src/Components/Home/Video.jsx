c306a5a101b6fa1554ec3764daff29c9b080d5f6
wayha1 <vehaseng1122@gmail.com>
2024-05-11 18:17:13 +0700

    updating video

--- src/Components/Home/Video.jsx
+++ src/Components/Home/Video.jsx
@@ -1,72 +1,55 @@
-import React from "react";
-import video1 from "../../asset/video/video1.mp4";
-import video2 from "../../asset/video/video2.mp4";
-import video3 from "../../asset/video/video3.mp4";
+import React, { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import axios from "../api/axios";
+
 function Video() {
+  const [videoData, setVideoData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const token = Cookies.get("token");
+
+      const response = await axios.request({
+        url: "/video",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("Fetching data...", response);
+      setVideoData(response.data.data);
+    } catch (error) {
+      console.error("Error fetching video data:", error);
+      setVideoData([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <div>
-        <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3  items-center justify-center gap-y-20 gap-x-14 pt-10 pb-5 text-center">
-          <div className="rounded-lg max-w-sm">
-            <a href="#!">
-              <video
+    <div>
+      <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 items-center justify-center gap-y-20 gap-x-14 pt-10 pb-5 text-center">
+        {videoData.map((video, index) => (
+          <div key={index} className="rounded-lg max-w-sm">
+            <a href={video.video_link}>
+              <iframe
+                src={video.video_link}
                 width="320"
                 height="240"
                 controls
-                className="w-full rounded-t-lg"
-              >
-                <source src={video1} type="video/mp4" />
-                {/* <source src="movie.ogg" type="video/ogg" /> */}
+                className="w-full rounded-t-lg">
                 Your browser does not support the video tag.
-              </video>
+              </iframe>
             </a>
             <div className="p-6">
-              <h5 className="text-gray-900 text-center text-sm font-medium mb-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing.
-              </h5>
+              <h5 className="text-gray-900 text-center text-sm font-medium mb-2">{video.video_title}</h5>
             </div>
           </div>
-          <div className="rounded-lg max-w-sm">
-            <a href="#!">
-              <video
-                width="320"
-                height="240"
-                controls
-                className="w-full rounded-t-lg"
-              >
-                <source src={video2} type="video/mp4" />
-                {/* <source src="movie.ogg" type="video/ogg" /> */}
-                Your browser does not support the video tag.
-              </video>
-            </a>
-            <div className="p-6">
-              <h5 className="text-gray-900 text-center text-sm font-medium mb-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing.
-              </h5>
-            </div>
-          </div>
-          <div className="rounded-lg max-w-sm">
-            <a href="#!">
-              <video
-                width="320"
-                height="240"
-                controls
-                className="w-full rounded-t-lg"
-              >
-                <source src={video3} type="video/mp4" />
-                {/* <source src="movie.ogg" type="video/ogg" /> */}
-                Your browser does not support the video tag.
-              </video>
-            </a>
-            <div className="p-6">
-              <h5 className="text-gray-900 text-center text-sm font-medium mb-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing.
-              </h5>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
