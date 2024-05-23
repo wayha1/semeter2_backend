520b7b1ca295375c97f09152c8ca467278a196df
ssdn7 <somsodana9@gmail.com>
2024-05-23 15:27:26 +0700

    update brand

---
+++ src/Dashboard/DashboardBrand.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Brandinformation from "./Brand/Brandinformation";
+import DashboardLayout from "./DashboardLayout";
+
+function DashboardBrand() {
+  return (
+    <>
+      <DashboardLayout>
+        <Brandinformation />
+      </DashboardLayout>
+    </>
+  );
+}
+
+export default DashboardBrand;
