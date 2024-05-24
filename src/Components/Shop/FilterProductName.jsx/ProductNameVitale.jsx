08dc8a897ce14d1fda6b6270ffbc5ca40560bb78
JackyDom <sopheaoudom8@gmail.com>
2024-05-24 22:26:35 +0700

    update_pagination

--- src/Components/Shop/FilterProductName.jsx/ProductNameVitale.jsx
+++ src/Components/Shop/FilterProductName.jsx/ProductNameVitale.jsx
@@ -3,17 +3,17 @@ import Cookies from "js-cookie";
 import { Link, useParams } from "react-router-dom";
 import axios from "../../api/axios";
 import { useNavigate } from "react-router-dom";
-import useAuthContext from "./../../context/AuthContext";
+import useAuthContext from "./../../context/AuthContext"
 import { CgArrowLeftO } from "react-icons/cg";
 
-function ProductNamePure() {
+
+function ProductNameVitale() {
   const { name } = useParams();
   const [product1, setProduct1] = useState([]);
   const { user } = useAuthContext();
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
   const [brandName, setBrandName] = useState("");
+
 
   const navigateToProductDetails = (
     productImage,
@@ -34,7 +34,7 @@ function ProductNamePure() {
         productStock,
         productReview,
         userId: user.id,
-        productId: productId,
+        productId: productId
       },
     });
   };
@@ -51,20 +51,17 @@ function ProductNamePure() {
         },
       });
       console.log("Response data:", response.data);
-      const luminousGlowProducts = response.data.data.filter(
-        (product) => product.product_brand === "Pure Radiance 2023"
+      const ElysianProducts = response.data.data.filter(
+        (product) => product.product_brand === "Vitalé Beauty 2023"
       );
-      setProduct1(luminousGlowProducts);
-      setLoading(false);
+      setProduct1(ElysianProducts);
 
       // Set brand name if products are available
-      if (luminousGlowProducts.length > 0) {
-        setBrandName(luminousGlowProducts[0].product_brand);
+      if (ElysianProducts.length > 0) {
+        setBrandName(ElysianProducts[0].product_brand);
       }
     } catch (error) {
       console.error("Error fetching user data:", error);
-      setError(error.message);
-      setLoading(false);
       setProduct1([]);
     }
   };
@@ -72,14 +69,6 @@ function ProductNamePure() {
   useEffect(() => {
     fetchData();
   }, [name]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
 
   return (
     <div>
@@ -94,11 +83,10 @@ function ProductNamePure() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 m-10">
         {product1.map((product) => (
           <div key={product.id} className="p-4 border rounded-lg">
-            <img
-              src={product.product_image}
-              alt={product.product_name}
-              className="max-w-full h-auto object-cover rounded-t-xl"
-            />
+            <img 
+            src={product.product_image} 
+            alt={product.product_name} 
+            className="max-sm:w-30 max-sm:h-50 lg:h-80 lg:w-72 object-cover rounded-t-xl"/>
             <h3 className="md:text-lg text-xs max-sm:text-sm font-medium mb-2">
               {product.product_brand}
             </h3>
@@ -108,25 +96,27 @@ function ProductNamePure() {
             <p className="text-gray-600 line-clamp-3 text-xs md:text-sm mb-4">
               {product.product_description}
             </p>
-            <div className="flex justify-between">
+            
+            <div className="flex justify-between"> {/* Flex container */}
               <span className="font-bold md:text-lg text-xs max-sm:text-sm">
                 ${product.product_price}
               </span>
               <button
-                onClick={() =>
-                  navigateToProductDetails(
-                    product.product_image,
-                    product.product_name,
-                    product.product_description,
-                    product.product_price,
-                    product.product_stock,
-                    product.product_review,
-                    product.id
-                  )
-                }
+              onClick={() => {
+                     
+                console.log("Product data:", product);
+                navigateToProductDetails(
+                  product.product_image,
+                  product.product_name,
+                  product.product_description,
+                  product.product_price,
+                  product.product_stock,
+                  product.product_review,
+                  product.id
+                );
+              }}
                 className="bg-pink-400 hover:bg-pink-600 
-                text-white font-bold py-2 px-4 max-sm:p-2 max-sm:text-xs rounded"
-              >
+                text-white font-bold py-2 px-4 max-sm:p-2 max-sm:text-xs rounded">
                 Buy Now
               </button>
             </div>
@@ -137,4 +127,4 @@ function ProductNamePure() {
   );
 }
 
-export default ProductNamePure;
+export default ProductNameVitale
