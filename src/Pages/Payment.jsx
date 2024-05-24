a5a488787bf8c9e1d1e4d18d23c007b43cf60c42
JackyDom <sopheaoudom8@gmail.com>
2024-05-24 15:37:59 +0700

    update

--- src/Pages/Payment.jsx
+++ src/Pages/Payment.jsx
@@ -1,18 +1,9 @@
-import React from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import CheckoutForm from '../Components/Payment/CheckoutForm';
+import React from 'react'
 
-const stripePromise = loadStripe('pk_test_51PH2vERvwUFIwe3TbN1v3XQaTAJ0pszrDD7S6ywzyNtnuqIDhTgjNFnYoMxzNdsfdzeHtf0VKaUk9doEeK7qwkAM00lKbVe9q2');
-
-const Payment = () => {
+function Payment() {
   return (
-    <Elements stripe={stripePromise}>
-      <div className="min-h-screen flex items-center justify-center">
-        <CheckoutForm />
-      </div>
-    </Elements>
-  );
-};
+    <div>Payment</div>
+  )
+}
 
-export default Payment;
+export default Payment
