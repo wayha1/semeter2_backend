1361afccbd0376f4dd51d4367b19a5558f06e165
ssdn7 <somsodana9@gmail.com>
2024-05-28 15:27:20 +0700

    update axios

--- src/Components/api/axios.js
+++ src/Components/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://13.251.94.255:8000/api",
   withCredentials: true,
   headers: {
     "Content-type": "application/json",
