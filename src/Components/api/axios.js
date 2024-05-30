829d63d6d30e322fd72506ab51bbaf4b0c228e1e
ssdn7 <somsodana9@gmail.com>
2024-05-30 17:07:51 +0700

    last updated

--- src/Components/api/axios.js
+++ src/Components/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://backend.skinme.site:8000/api",
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://backend.skinme.site:8000/api",
+  // baseURL: "http://127.0.0.1:8000/api",
   withCredentials: true,
   headers: {
     "Content-type": "application/json",
